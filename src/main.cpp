cad9018cea78d5eed32a0524da90f96f9625c86d
ThomasGnida <114877277+ThomasGnida@users.noreply.github.com>
2025-12-08 15:36:59 +0100

    Update Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*#include <Adafruit_GFX.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -8,7 +8,7 @@
 /*
     If everything works as it should, the display should display which sensors are operational or not.
     The same information should be shown in the serial monitor as well @115200 baud.
-#1#
+*/
 // If wired identical to the image in the moodle course, these pin definitions are correct.
 // Otherwise, adapt as neccessary.
 #define TFT_CS D3
@@ -21,6 +21,9 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 // --- MAX30102 PINS on I2C1 (SDA=D4, SCL=D5) ---
 MAX30105 HRSensor;
 TwoWire myWire(&i2c1_inst, D4, D5);
+static uint16_t lastDistance = 0;
+static uint16_t currentDistance = 0;
+static uint16_t lastMeasurementTime = 0;
 
 
 
@@ -28,57 +31,36 @@ void setup() {
   Serial.begin(115200);
   delay(3000);
   Serial.println("Initializing modules...");
-
-  // --- LCD Test ---
-  tft.init(240, 240);
-  tft.setRotation(2);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(2);
-  tft.setCursor(10, 20);
-  tft.println("Screen operational");
-  delay(1000);
-
-  // --- Ultrasonic Test ---
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(TRIG_PIN, OUTPUT);
-
+}
+float measureDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-
-  if (pulseIn(ECHO_PIN, HIGH, 3000000) > 0) {
-    tft.setTextColor(ST77XX_WHITE);
-    tft.setCursor(10, 100);
-    tft.println("DistanceSensor operational");
-    Serial.println("DistanceSensor operational");
-  } else {
-    tft.setCursor(40, 100);
-    tft.setTextColor(ST77XX_RED);
-    tft.println("DistanceSensor FAIL OR TIMEOUT!");
-    Serial.println("DistanceSensor FAIL OR TIMEOUT!");
+  
+  uint32_t duration = pulseIn(ECHO_PIN, HIGH, 1000000);
+  if (duration == 0){
+    return -1; // Indicate timeout or no reading
   }
-
-  // --- MAX30102 Test ---
-  myWire.begin();
-  if (!HRSensor.begin(myWire, I2C_SPEED_STANDARD)) {
-    tft.setCursor(40, 180);
-    tft.setTextColor(ST77XX_RED);
-    tft.println("MAX30102 not found. Check wiring!");
-    Serial.println("MAX30102 not found. Check wiring!");
-    while (1)
-      ;
-  } else {
-    tft.setCursor(40, 180);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.println("MAX30102 operational");
-    Serial.println("MAX30102 operational");
-  }
+  float distance_cm = duration * 0.0343 / 2; //cm/us divided by 2 for go and return
+  return distance_cm;
 }
 
+float measureSpeed(float oldDistance, float newDistance, float secondsInterval) {
+  if (secondsInterval <= 0) {
+    return 0; // Prevent division by zero
+  }
+  return (newDistance - oldDistance) / secondsInterval; // cm/s positive values = approaching, negative = receding
+}
+
+
 void loop() {
+  unsigned long currentTime = millis();
+  lastDistance = currentDistance;
+  currentDistance = measureDistance();
+  Serial.print("Distance (cm): " + String(currentDistance) + " | Speed (cm/s): " + String(measureSpeed(lastDistance, currentDistance, (currentTime - lastMeasurementTime) / 1000.0)) );
+  lastMeasurementTime = currentTime;
 
   delay(1);
-}*/
+}
