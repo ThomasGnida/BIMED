0b2cbae097ba5e50a8339972676a901151f5ea3b
Thomas Gnida <114877277+ThomasGnida@users.noreply.github.com>
2025-12-10 22:47:33 +0100

    Further Refractor and cleanup

--- src/Assignment.cpp
+++ src/Assignment.cpp
@@ -7,25 +7,18 @@
 #include "spo2_algorithm.h"
 #include <math.h>
 
-#define TFT_CS D3
-#define TFT_RST D2
-#define TFT_DC D7
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_ST7789 screen = Adafruit_ST7789(D3, D7, D2);
 #define ECHO_PIN D0
 #define TRIG_PIN D1
-
-// --- MAX30102 PINS on I2C1 (SDA=D4, SCL=D5) ---
 MAX30105 HRSensor;
 TwoWire myWire(&i2c1_inst, D4, D5);
 
 // Distance tracking
 float lastDistance = 0.0;
 float currentDistance = 0.0;
-static unsigned long lastDistanceRead = 0;
-unsigned long lastMeasurementTime = 0;
-float total_distance = 0.0;
+uint16_t lastDistanceTime = 0;
+float totalDistance = 0.0;
 float currentSpeed = 0.0;
-
 
 // SpO2 calculation buffers
 #define BUFFER_LENGTH 100
@@ -39,9 +32,10 @@ bool fingerDetected = false;
 
 uint8_t bufferIndex = 0;
 bool bufferReady = false;
-uint32_t lastHeartRateCalc = 0;
+uint32_t lastHeartRateCalcTime = 0;
 uint16_t HR_CALC_INTERVAL = 500;
 uint32_t lastDisplayUpdate = 0;
+
 
 
 float measureDistance() {
@@ -50,19 +44,10 @@ float measureDistance() {
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  
   uint32_t duration = pulseIn(ECHO_PIN, HIGH, 30000);
-  if (duration == 0){
-    return -1; // Indicate timeout or no reading
-  }
-  float distance_cm = duration * 0.0343 / 2; //cm/us divided by 2 for go and return
-  return distance_cm;
-}
 
-float measureSpeed(float oldDistance, float newDistance, float secondsInterval) {
-  return (newDistance - oldDistance) / secondsInterval; // cm/s positive values = approaching, negative = receding
+  return duration * 0.0343 / 2; //cm/us divided by 2 for go and return
 }
-
 
 bool collectSensorSample() {
   uint32_t redValue = HRSensor.getRed();
@@ -72,9 +57,8 @@ bool collectSensorSample() {
   if (redValue > 15000 && irValue > 15000) {
     redBuffer[bufferIndex] = redValue;
     irBuffer[bufferIndex] = irValue;
-    
     if(!bufferReady & bufferIndex +1 == BUFFER_LENGTH){
-      bufferReady = true; //Arrays are filled for the first time
+      bufferReady = true; 
     }
     bufferIndex = (bufferIndex + 1) % BUFFER_LENGTH;
     return true;
@@ -83,74 +67,59 @@ bool collectSensorSample() {
 }
 
 void updateDisplay() {
-    lastDisplayUpdate = millis();
-
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setTextSize(2);
-    tft.setTextColor(ST77XX_WHITE);
-
-    // Display HR and SpO2
+  screen.fillScreen(ST77XX_BLACK);
     if (!fingerDetected){
-        tft.setCursor(10, 20);
-        tft.println("No finger detected");
+        screen.setCursor(10, 20);
+        screen.println("No finger detected");
     } else if (validSPO2 && validHeartRate) {
-        tft.setCursor(10, 20);
-        tft.print("  HR: ");
-        tft.print(heartRate);
-        tft.println(" bpm");
+        screen.setCursor(10, 20);
+        screen.print("  HR: ");
+        screen.print(heartRate);
+        screen.println(" bpm");
 
-        tft.setCursor(10, 50);
-        tft.print("SpO2: ");
-        tft.print(spo2);
-        tft.println(" %");
-
+        screen.setCursor(10, 50);
+        screen.print("SpO2: ");
+        screen.print(spo2);
+        screen.println(" %");
     } else {
-        tft.setCursor(10, 20);
-        tft.println("Invalid HR/Spo2 values");
+        screen.setCursor(10, 20);
+        screen.println("Invalid Spo2 values");
     }
 
-    // Display Distance
-    tft.setCursor(10, 100);
+    screen.setCursor(10, 100);
     if (currentDistance >= 0) {
-        tft.print("Dist: ");
-        tft.print(currentDistance, 1);
-        tft.println(" cm");
-
+        screen.print("Dist: ");
+        screen.print(currentDistance, 1);
+        screen.println(" cm");
     } else {
-        tft.println("Dist: --");
+        screen.println("Dist: --");
     }
-
-    // Display Speed
-    tft.setCursor(10, 130);
-    tft.print("Speed: ");
-    tft.print(currentSpeed, 1);
-    tft.println(" cm/s");
-
-    tft.setCursor(10, 160);
-    tft.print("Total Dist: ");
-    tft.print(total_distance, 2); // convert to meters
-    tft.println(" cm");
+    
+    screen.setCursor(10, 130);
+    screen.print("Speed: ");
+    screen.print(currentSpeed, 1);
+    screen.println(" cm/s");
+    screen.setCursor(10, 160);
+    screen.print("Total Dist: ");
+    screen.print(totalDistance, 1);
+    screen.println(" cm");
 }
 
 void setup() {
-  static bool initialized = false;
-  // Basic serial + display initialization first so we can show errors
+  bool initialized = false;
   Serial.begin(115200);
   delay(300);
-  tft.init(240, 240);
-  tft.setRotation(2);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(2);
-  Serial.println("Initializing modules...");
-
+  screen.init(240, 240);
+  screen.setRotation(2);
+  screen.fillScreen(ST77XX_BLACK);
+  screen.setTextColor(ST77XX_WHITE);
+  screen.setTextSize(2);
   // --- I2C / MAX30102 ---
   if (!initialized) {
     myWire.begin();
     if (!HRSensor.begin(myWire, I2C_SPEED_STANDARD)) {
-      tft.fillScreen(ST77XX_BLACK);
-      tft.setCursor(0, 0);
-      tft.println("MAX30102 not found!");
+      screen.setCursor(0, 0);
+      screen.println("MAX30102 not found!");
       while (1);
     }
     HRSensor.setup(0x2F, 4, 2, 50, 411, 4096);
@@ -164,7 +133,7 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   digitalWrite(TRIG_PIN, LOW);
 
-  lastMeasurementTime = millis();
+  lastDistanceTime = millis();
 }
 
 void loop() {
@@ -172,8 +141,8 @@ void loop() {
   fingerDetected = collectSensorSample();
   
   // Calculate HR and SpO2 every 500ms when buffer is full and ready
-  if (bufferReady && (currentTime - lastHeartRateCalc >= HR_CALC_INTERVAL)) {
-    lastHeartRateCalc = currentTime;
+  if (bufferReady && (currentTime - lastHeartRateCalcTime >= HR_CALC_INTERVAL)) {
+    lastHeartRateCalcTime = currentTime;
     
     maxim_heart_rate_and_oxygen_saturation(
       irBuffer, 
@@ -186,26 +155,26 @@ void loop() {
     );
   }
   
-  if (currentTime - lastDistanceRead >= 100) { // Every 100ms
-    lastDistanceRead = currentTime;
+  if (currentTime - lastDistanceTime >= 100) { // Every 100ms
     
     float newDistance = measureDistance();
     
     if (newDistance >= 0.0) {
-      float timeDifference = (currentTime - lastMeasurementTime) / 1000.0;
+      float timeDifference = (currentTime - lastDistanceTime) / 1000.0;
       
       if (timeDifference > 0) {
-        currentSpeed = measureSpeed(lastDistance, newDistance, timeDifference);
+        currentSpeed = (newDistance - lastDistance) / timeDifference;
+
         float distanceDelta = abs(newDistance - lastDistance);
         
         if (distanceDelta > 0.5 && distanceDelta < 100) { // Ignore noise and unrealistic jumps
-          total_distance += distanceDelta;
+          totalDistance += distanceDelta;
         }
       }
      
       currentDistance = newDistance;
       lastDistance = newDistance;
-      lastMeasurementTime = currentTime;
+      lastDistanceTime = currentTime;
     }
   }
 
